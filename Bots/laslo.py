66b9ee72351fa53a40aad86152fe4000a2278f67
ijp74190 <ijp74190@ucmo.edu>
2024-09-28 12:17:41 -0500

    Add checks for valid moves

--- Bots/laslo.py
+++ Bots/laslo.py
@@ -9,8 +9,18 @@ class Player:
 			self.player = 2
 			self.oppo = 1
 
+
 	def makeMove(self, gameState) -> int:
-		if self.player == 1:
-			return 0
-		else:
-			return 6
+		for col in range(6):
+			if self.validMove(gameState, col):
+				return col
+		return 6
+
+
+	def validMove(self, gameState, col):
+		if col < 0 or col > 6:
+			return False
+		for row in range(6):
+			if gameState[row][col] == 0:
+				return True
+		return False
