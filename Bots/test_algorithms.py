bdb46e44364b2a2df6746a655b540fd930230280
ijp74190 <ijp74190@ucmo.edu>
2024-09-28 22:43:10 -0500

    created shame

--- Bots/test_algorithms.py
+++ Bots/test_algorithms.py
@@ -2,6 +2,7 @@ import numpy as np
 from Bots.VAMPIRE import Player as VampirePlayer
 from Bots.guillermo import Player as GuillermoPlayer
 from Bots.werewolf import Player as WerewolfPlayer
+from Bots.shame import Player as ShamePlayer
 
 def print_board(board):
     print(np.flip(board, 0))
@@ -12,8 +13,8 @@ def play_game():
     turn = 0
     play = 0
 
-    player1 = WerewolfPlayer(playerOne=True)  # Player 1 using VAMPIRE
-    player2 = WerewolfPlayer(playerOne=False)  # Player 2 using guillermo
+    player1 = VampirePlayer(playerOne=True)  # Player 1 using VAMPIRE
+    player2 = ShamePlayer(playerOne=False)  # Player 2 using guillermo
 
     while not game_over:
         if turn == 0:
