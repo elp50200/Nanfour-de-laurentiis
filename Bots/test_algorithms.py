915f97eb8265f4b529fd68b3cac36a1e03689d5f
elp50200 <elp50200@ucmo.edu>
2024-09-28 21:09:15 -0500

    testing commends

--- Bots/test_algorithms.py
+++ Bots/test_algorithms.py
@@ -9,6 +9,7 @@ def play_game():
     board = np.zeros((6, 7), dtype=int)
     game_over = False
     turn = 0
+    play = 0
 
     player1 = VampirePlayer(playerOne=True)  # Player 1 using VAMPIRE
     player2 = GuillermoPlayer(playerOne=False)  # Player 2 using guillermo
@@ -17,6 +18,7 @@ def play_game():
         if turn == 0:
             col = player1.makeMove(board)
             if player1.is_valid_location(board, col):
+                print("Player 1 (VAMPIRE) chose column", col)
                 row = player1.get_next_open_row(board, col)
                 player1.drop_piece(board, row, col, player1.player)
                 if player1.detect_win(board, player1.player):
@@ -25,13 +27,16 @@ def play_game():
         else:
             col = player2.makeMove(board)
             if player2.is_valid_location(board, col):
+                print("Player 2 (guillermo) chose column", col)
                 row = player2.get_next_open_row(board, col)
                 player2.drop_piece(board, row, col, player2.player)
                 if player2.detect_win(board, player2.player):
                     print("Player 2 (guillermo) wins!")
                     game_over = True
 
+        play += 1
         print_board(board)
+        print(play)
         print("\n")
 
         turn += 1
